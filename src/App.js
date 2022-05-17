a2bbefad61e1e7146fb81d25aaf44ce1eaca192e
Vivek Kumar Singh <kumarsinghvivek27951@gmail.com>
2022-05-17 21:49:52 +0530

    detail page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Stocks from "./components/Stocks";
 import Gold from "./components/Gold";
--- src/components/DetailStock.js
+++ src/components/DetailStock.js
@@ -33,20 +33,64 @@ const DetailStock = () => {
     // console.log("comp", currStock);
   }, []);
   return (
-    <div>
-      <div className="">
+    <div className="flex flex-col items-center">
+      <div>
         <img
           className="mx-4 my-4"
           src={currStock ? currStock.company.imageUrl : null}
           alt="company logo"
         />
-        <br />
-        <h2>{currStock ? currStock.company.companyName : null}</h2>
-        <br />
-        <div className="">
-          <p>LTP: {currStock ? currStock.stats.ltp : null}</p>
-          <p>HIGH: {currStock ? currStock.stats.high : null}</p>
-          <p>LOW: {currStock ? currStock.stats.low : null}</p>
+        <h2 className="flex justify-center text-3xl font-medium tracking-wide text-blue-600 underline">
+          {currStock ? currStock.company.companyName : null}
+        </h2>
+      </div>
+      <div className="p-10 mt-20 text-lg">
+        <div className="flex justify-around m-5 space-x-16">
+          <p className="text-[#00d09c]">
+            <span className="text-gray-600">Open :</span> <span>&#8377;</span>
+            {currStock ? currStock.stats.open : null}
+          </p>
+          <p className="text-[#00d09c]">
+            <span className="text-gray-600">Ltp :</span> <span>&#8377;</span>
+            {currStock ? currStock.stats.ltp : null}
+          </p>
+          <p className="text-[#00d09c]">
+            <span className="text-gray-600">High :</span> <span>&#8377;</span>
+            {currStock ? currStock.stats.high : null}
+          </p>
+          <p className="text-[#00d09c]">
+            <span className="text-gray-600">Low :</span> <span>&#8377;</span>
+            {currStock ? currStock.stats.low : null}
+          </p>
+          <p className="text-[#00d09c]">
+            <span className="text-gray-600">Close :</span> <span>&#8377;</span>
+            {currStock ? currStock.stats.close : null}
+          </p>
+        </div>
+        <div className="flex justify-around m-5 space-x-16">
+          <p className="text-[#00d09c]">
+            <span className="text-gray-600">Low Price Range :</span>{" "}
+            <span>&#8377;</span>
+            {currStock ? currStock.stats.lowPriceRange : null}
+          </p>
+          <p className="text-[#00d09c]">
+            <span className="text-gray-600">High Price Range :</span>{" "}
+            <span>&#8377;</span>
+            {currStock ? currStock.stats.highPriceRange : null}
+          </p>
+          <p className="text-[#00d09c]">
+            <span className="text-gray-600">Market Cap :</span>{" "}
+            <span>&#8377;</span>
+            {currStock ? currStock.stats.marketCap : null}
+          </p>
+        </div>
+        <div className="flex justify-center">
+          <p className="text-[#00d09c]">
+            <span className="text-gray-600">Day Change :</span>{" "}
+            <span>&#8377;</span>
+            {currStock ? currStock.stats.dayChange : null} (
+            {currStock ? currStock.stats.dayChangePerc : null}%)
+          </p>
         </div>
       </div>
     </div>
